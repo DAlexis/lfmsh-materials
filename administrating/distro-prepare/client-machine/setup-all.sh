22f9feb737df560c78292fe9ce160dfc57bea4e2
Alexey Bulatov <AlexeyABulatov@yandex.ru>
2022-09-28 22:46:55 +0300

    Data of \'35 added

---
+++ administrating/distro-prepare/client-machine/setup-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ssh administrator@school-2.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-3.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-4.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-5.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-6.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-7.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-8.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-9.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-10.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-11.local 'bash -s' < setup-syncthing.sh
+ssh administrator@school-12.local 'bash -s' < setup-syncthing.sh
