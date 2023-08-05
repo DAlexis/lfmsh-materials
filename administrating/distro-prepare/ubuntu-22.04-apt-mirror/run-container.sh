733fb83189502bc5784ca9bf59a9fe409712bc31
Alexey Bulatov <AlexeyABulatov@yandex.ru>
2023-08-06 02:23:28 +0300

    actual state of docker before '36

--- administrating/distro-prepare/ubuntu-22.04-apt-mirror/run-container.sh
+++ administrating/distro-prepare/ubuntu-22.04-apt-mirror/run-container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run -it --rm \
-  --mount type=bind,source=/mnt/data-ext4-int/ubuntu-22.04-mirror,target=/mnt/mirror \
+  --mount type=bind,source=/home/dalexies/repo-mirror/ubuntu-22.04-mirror,target=/mnt/mirror \
   --user $(id -u):$(id -g) \
   --log-driver none \
   --volume /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro \
